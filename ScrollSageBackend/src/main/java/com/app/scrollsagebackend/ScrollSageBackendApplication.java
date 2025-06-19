3c0a1f74f46405dc81436f5a29f945259b2996a6
GHYounesse <ghalloumyounes2003@gmail.com>
2025-06-20 00:28:28 +0100

    First Commit

---
+++ ScrollSageBackend/src/main/java/com/app/scrollsagebackend/ScrollSageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.scrollsagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrollSageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScrollSageBackendApplication.class, args);
+    }
+
+}
